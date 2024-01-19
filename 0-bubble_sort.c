9c4346f1efda37293e57b54424ad733b9aeb601a
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-01-19 12:15:34 -0800

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,28 @@
 #include "sort.h"
 
 /**
- * bubble_sort - trie les éléments du tableau de la plus petite valeur à la plus grande valeur
- * @arr : Le tableau
- * @n : La taille du tableau
+ * bubble_sort - trie les ele du tableau de la plus petite à la plus grande
+ * @arr: Le tableau
+ * @n: La taille du tableau
  */
 void bubble_sort(int *arr, size_t n)
 {
-    size_t i, j, temp = 0;
+	size_t i, j, temp = 0;
 
-    if (n < 2)
-        return;
+	if (n < 2)
+		exit(EXIT_FAILURE);
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            if (arr[j] > arr[j + 1] && arr[j + 1])
-            {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                print_array(arr, n);
-            }
-        }
-    }
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (arr[j] > arr[j + 1] && arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				print_array(arr, n);
+			}
+		}
+	}
 }
